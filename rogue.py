f91f1399d8617d72b42be2d9447c108efb4c2288
LaAlice <larissapusch@gmx.net>
2018-01-22 13:38:51 +0100

    deletion and a bit of other stuff

--- rogue.py
+++ rogue.py
@@ -7,14 +7,17 @@ import settings as s
 
 tdl.set_font('celtic_garamond_10x10_gs_tc.png', greyscale=True, altLayout=True)
 
-console = tdl.init(s.SCREEN_WIDTH, s.SCREEN_HEIGHT, title="PyRogue", fullscreen = False)
+root = tdl.init(s.SCREEN_WIDTH, s.SCREEN_HEIGHT, title="PyRogue", fullscreen = False)
+con = tdl.Console(s.SCREEN_WIDTH, s.SCREEN_HEIGHT)
+
 tdl.set_fps(s.LIMIT_FPS)
 
 while not tdl.event.is_window_closed():
-    console.draw_char(s.player_x,s.player_y,'a', bg=None, fg=(255,255,255))
+    con.draw_char(s.player_x,s.player_y,'a', bg=None, fg=(255,255,255))
 
+    tdl.flush() #always execute at end of loop; presents changes to screen
+
+    con.draw_char(s.player_x, s.player_y, ' ', bg=None )
     exit_game = handle_keys()
     if exit_game:
         break
-
-    tdl.flush() #always execute at end of loop; presents changes to screen
